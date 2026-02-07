de1fd80b284f853c89fb05add42376370f850c5a
siteforcouplesvalentine-jpg <siteforcouplesvalentine@gmail.com>
2026-02-07 21:10:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -96,3 +96,8 @@ async function startDragonBot() {
 }
 
 startDragonBot();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('Dragon is Alive!'));
+app.listen(port, () => console.log(`Server running on port ${port}`));
